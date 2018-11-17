e0ee2c070cc9707e158785df7d540c9a96cf14b5
Giulio Caleffi <caleffigiulio@gmail.com>
2018-11-17 20:45:22 +1000

    updated api method to fetch from axios

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const axios = require("axios")
-const fs = require('fs')
-
-const baseURL = "https://gateway.marvel.com"
-const api_key = "2b98cdfb6265c41152a598bb461ab0ca"
-
-axios.get(baseURL + "/443/v1/public/characters?apikey=" + api_key).then((result) => {
-  var characters = result.data
-  fs.writeFileSync(`./src/data/characters.json`, JSON.stringify(characters, ``, 2))
-})
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://gateway.marvel.com:443/v1/public/characters?apikey=2b98cdfb6265c41152a598bb461ab0ca")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
   render() {
     return (
       <div className="App">
